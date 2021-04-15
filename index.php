63b3f7c87a71674809d1bd4e8d9dc8a75a73835c
jacobkalat <61515665+jacobkalat@users.noreply.github.com>
2021-04-15 11:44:31 -0400

    Adds basic functionality
    Adds search field, submit, and reset button to index.html. Index.php receives the search term and the database is linked to this file. The beginning of the table is created with styling.

--- index.php
+++ index.php
@@ -1,1 +1,52 @@
-<?php  ?>
+<!--	Names: 	 Brian Hafey, Jacob Kalat, Maria Leyva
+      	Date: 	 4/15/2021
+      	Purpose: To recieve search term and query database for all rows including that term. Print results in table format-->
+
+<!-- Declares style for table -->
+<style>
+    table{
+        background-color:#DDEEFF;
+        border:1px solid black;
+        padding: 3px;
+    }
+	th{
+		background-color:#DDEEFF;
+        border:1px solid black;
+        padding: 3px;
+	}
+	tr{
+		background-color:#DDEEFF;
+        border:1px solid black;
+        padding: 3px;
+	}
+	td{
+		background-color:#DDEEFF;
+        border:1px solid black;
+        padding: 3px;
+	}
+</style>
+
+<?php  
+//Set search term from POST
+$search = $_POST["search"];
+
+//Print search term to be sure it was passed correctly
+echo "Search Term = '$search'";
+
+//Link to mysql database. May have to change port and database. Then checks if connect failed. If so exit.
+$link = new mysqli("localhost:3306","root","root","hello");
+if(mysqli_connect_error())
+{
+		die("connection failed: ".mysqli_connect_error());
+}
+
+//Queries all rows from personnelInfo table in hello database. Stores result and number of rows
+$query = "SELECT * from personnelInfo";
+$result = mysqli_query($link,$query);
+$numOfRows = mysqli_num_rows($result);
+
+//Print the header for the table 
+echo "<table><tr><th>Full Name</th><th>Photos</th><th>Date Joined</th><th>Department</th><th>Project Involved</th><th>Annual Salary</th></tr>";
+
+echo "</table>"
+?>
